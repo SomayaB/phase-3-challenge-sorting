f69dc2513dffa0397bdae3db2e24a38cd2422101
SomayaB <somaya.bounouar@gmail.com>
2017-06-19 15:19:20 -0700

    Add specific colum names to be loaded

--- part-2/load-data.sql
+++ part-2/load-data.sql
@@ -1,5 +1,5 @@
 \c grocery_store
 
-COPY grocery_items FROM '/Users/Somaya/Desktop/LGProjects/phase-3-challenge/part-2/grocery.csv' DELIMITER ',' CSV HEADER;
+COPY grocery_items (name, price, section) FROM '/Users/Somaya/Desktop/LGProjects/phase-3-challenge/part-2/grocery.csv' DELIMITER ',' CSV HEADER;
 
 SELECT * FROM grocery_items;
