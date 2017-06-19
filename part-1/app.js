4b1c120d4c283d43d639e82c65b7642e8463bef0
SomayaB <somaya.bounouar@gmail.com>
2017-06-19 10:29:24 -0700

    Add route /zero

--- part-1/app.js
+++ part-1/app.js
@@ -2,6 +2,11 @@ var express = require("express")
 
 var app = express()
 
+app.get("/zero", function(req, res){
+  res.send("0")
+})
+
+
 
 app.listen(3000, function(){
   console.log("Listening on Port 3000");
