6abe37d44afe74e47dfdf4bf85e9f5fc841d0a5c
SomayaB <somaya.bounouar@gmail.com>
2017-06-19 16:19:39 -0700

    Insert rows into orders table

--- part-2/schema.sql
+++ part-2/schema.sql
@@ -3,6 +3,7 @@ CREATE DATABASE grocery_store;
 
 \c grocery_store;
 
+DROP TABLE IF EXISTS grocery_items;
 CREATE TABLE grocery_items (
   id SERIAL PRIMARY KEY,
   name TEXT,
@@ -10,24 +11,29 @@ CREATE TABLE grocery_items (
   section TEXT
 );
 
+DROP TABLE IF EXISTS shoppers;
 CREATE TABLE shoppers (
   id SERIAL PRIMARY KEY,
   name TEXT
 );
 
+DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
-  order_id SERIAL PRIMARY KEY,
-  shopper_id INTEGER
-);
-
-
-CREATE TABLE order_items (
-  order_id INTEGER,
+  id SERIAL PRIMARY KEY,
+  shopper_id INTEGER,
   item_id INTEGER,
   quantity INTEGER
 );
 
 
+-- SELECT * FROM grocery_items
+--   JOIN orders
+--   ON grocery_items.id = orders.item_id
+
 INSERT INTO shoppers(name)
   VALUES
   ('Alex'), ('Sophie'), ('Charlie'), ('Mary'), ('Tim');
+
+INSERT INTO orders(shopper_id, item_id, quantity)
+  VALUES
+  (1, 23, 2), (2, 16, 1), (3, 25, 5), (4, 30, 6), (5, 41, 2);
