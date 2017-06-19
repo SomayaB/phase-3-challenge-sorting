801eb37ac31ed025688bfaafaf411ada091b0918
SomayaB <somaya.bounouar@gmail.com>
2017-06-19 10:52:12 -0700

    Add route /add

--- part-1/app.js
+++ part-1/app.js
@@ -6,8 +6,12 @@ app.get("/zero", function(req, res){
   res.send("0")
 })
 
+app.get("/add", function(req, res){
+  var addition = Number(req.query.a) + Number(req.query.b)
+  res.send(String(addition))
+})
 
 
 app.listen(3000, function(){
-  console.log("Listening on Port 3000");
+  console.log("Listening on Port 3000")
 })
