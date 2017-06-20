55a7a44189f738ecf7dad533e28544b271d62f93
SomayaB <somaya.bounouar@gmail.com>
2017-06-20 09:37:47 -0700

    Create itemsInSection query function and change allItems function to function declaration

--- part-2/database.js
+++ part-2/database.js
@@ -3,4 +3,10 @@ var connectionString = 'postgres://localhost:5432/grocery_store'
 var db = pgp(connectionString)
 
 
-var allItems = db.any('SELECT * FROM grocery_items')
+function allItems() {
+  db.any('SELECT * FROM grocery_items')
+}
+
+function itemsInSection(section) {
+  db.any('SELECT id, name FROM grocery_items WHERE section = $1', section)
+}
