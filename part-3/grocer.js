02e77d74f7bf8c443b4f55f50eefa874b913b674
SomayaB <somaya.bounouar@gmail.com>
2017-06-20 14:58:32 -0700

    Add basic modal without data

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,6 +1,5 @@
 
 var addToCartButtons = document.querySelectorAll('.addToCartBtn')
-// "#cart-item-count"
 var cartItemCount = 0
 
 
@@ -10,3 +9,33 @@ for(var i = 0; i < addToCartButtons.length; i++){
   document.querySelector("#cart-item-count").textContent = '(' + cartItemCount + ')'
   })
 }
+
+
+// Get the modal
+var cartModal = document.querySelector('.cartModal');
+
+// Get the button that opens the modal
+var cartButton = document.querySelector('#cart-button');
+
+// Get the <span> element that closes the modal
+var span = document.querySelector(".close");
+
+// When the user clicks on the button, open the modal
+cartButton.addEventListener("click", function(){
+  cartModal.style.display = "block";
+})
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener("click", function(){
+  cartModal.style.display = "none";
+})
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", function(event){
+  if (event.target == cartModal) {
+      cartModal.style.display = "none";
+  }
+})
+
+
+//when add to cart is clicked, add textcontent of the item clicked to li inside cartModal div (this will be hidden until modal is opened)
