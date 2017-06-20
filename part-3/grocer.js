8f881e045a3a049f76e3a3ae3150f3132ccff2dd
SomayaB <somaya.bounouar@gmail.com>
2017-06-20 14:36:51 -0700

    Add 'Add to cart' functionality that updates the Cart counter

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,1 +1,12 @@
-console.log('CONNECTED')
+
+var addToCartButtons = document.querySelectorAll('.addToCartBtn')
+// "#cart-item-count"
+var cartItemCount = 0
+
+
+for(var i = 0; i < addToCartButtons.length; i++){
+  addToCartButtons[i].addEventListener("click", function(){
+  cartItemCount++;
+  document.querySelector("#cart-item-count").textContent = '(' + cartItemCount + ')'
+  })
+}
