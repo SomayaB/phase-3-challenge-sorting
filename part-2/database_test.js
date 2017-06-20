c7f647e28b3993cb2cce60f0198f100ef8668d74
SomayaB <somaya.bounouar@gmail.com>
2017-06-20 13:09:16 -0700

    Add test for countItemsInSection

--- part-2/database_test.js
+++ part-2/database_test.js
@@ -16,10 +16,21 @@ describe('itemsInSection()', function() {
 })
 
 describe('cheapItems()', function() {
-	it('should Find the IDs and prices of all grocery items that cost less than $10.00, ordered from lowest to highest price', function(done) {
+	it('should find the IDs and prices of all grocery items that cost less than $10.00, ordered from lowest to highest price', function(done) {
     db.cheapItems().then(function(result){
       expect(result[0]).to.eql({id: 15, price: '0.49'})
       expect(result[result.length - 1]).to.eql({id: 22, price: '9.31'})
+      done()
+    }).catch(function(error){
+      console.log('Whoops, my function doesnt behave as expected.', error)
+    })
+  })
+})
+
+describe('countItemsInSection()', function() {
+	it('should get the count of all grocery items in the given section', function(done) {
+    db.countItemsInSection('packaged').then(function(result){
+      expect(result[0].number_of_items_in_section).to.eql('5')
       done()
     }).catch(function(error){
       console.log('Whoops, my function doesnt behave as expected.', error)
