37a1df3e968f2d8912e21408146810b9581f6c93
SomayaB <somaya.bounouar@gmail.com>
2017-06-20 18:42:21 -0700

    Display Cart Total

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -2,6 +2,7 @@
 var addToCartButtons = document.querySelectorAll('.addToCartBtn')
 var cartItemCount = 0
 var cartModalContent = document.querySelector(".cartModalContent div")
+var cartTotal = 0
 
 for(var i = 0; i < addToCartButtons.length; i++){
   addToCartButtons[i].addEventListener("click", function(){
@@ -9,6 +10,10 @@ for(var i = 0; i < addToCartButtons.length; i++){
     document.querySelector("#cart-item-count").textContent = '(' + cartItemCount + ')'
     cartModalContent.appendChild(document.createElement('p')).textContent=this.parentNode.childNodes[1].textContent
     cartModalContent.appendChild(document.createElement('li')).textContent=this.parentNode.childNodes[3].textContent
+
+    var price = Math.round(Number((this.parentNode.childNodes[3].textContent).replace(/[^0-9\.]+/g,"")) * 100) / 100
+    cartTotal += price
+    document.querySelector(".total").textContent = cartTotal
   })
 }
 
